ab4e749932dbbe46541439fb1a6e23b3ce1c2778
Abhigna <abhignanshetty12@gmail.com>
2026-03-11 10:40:22 +0530

    AIRMAN Cloud Ops assessment project

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, jsonify
 import logging
+import sys
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    stream=sys.stdout,
+    format="%(asctime)s | %(levelname)s | %(message)s"
+)
 
 @app.route("/")
 def home():
+    print("Home endpoint accessed")
     logging.info("Home endpoint accessed")
     return jsonify({"message": "AIRMAN Cloud Ops Service Running"})
 
 @app.route("/health")
 def health():
+    print("Health check endpoint accessed")
     logging.info("Health check endpoint accessed")
-    return jsonify({"status": "healthy"}), 200
+    return jsonify({"status": "healthy"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
